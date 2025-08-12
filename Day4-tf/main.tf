b3074db0c6cb6c91fd7c9640478ddad7336c6774
saisureshmatru <saisureshmatru@gmail.com>
2025-08-12 11:57:00 +0530

    ertyui

---
+++ Day4-tf/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "example" {
+  ami           = "" # Amazon Linux 2 AMI for us-east-1
+  instance_type = "t3.micro"              # Free tier eligible
+
+  tags = {
+    Name = "server"
+  }
+}
+
