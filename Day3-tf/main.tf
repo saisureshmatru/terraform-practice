7d4b4e766fc3a23d92259eab1235076cba2ae949
saisureshmatru <saisureshmatru@gmail.com>
2025-08-12 11:54:48 +0530

    sdfghjkl

---
+++ Day3-tf/main.tf
@@ -0,0 +1,34 @@
+ource "aws_iam_role" "ec2_role" { 
+  name = "ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_instance_profile" {
+  name = "ec2-instance-profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-084a7d336e816906b"
+  instance_type = "t3.micro"
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.name
+
+  tags = {
+    Name = "ec2"
+  }
+}
+
+
